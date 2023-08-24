e78122b568f5f92ea310130d5fad06b8c3e3f1ea
Micah Pawlicki <mich.pawlicki@gmail.com>
2023-08-24 12:37:06 -0500

    start def badge implementation

--- src/app/type-badge/type-badge.component.ts
+++ src/app/type-badge/type-badge.component.ts
@@ -11,10 +11,10 @@ export class TypeBadgeComponent {
   @Input() typeOneBadge = '';
   @Input() typeTwoBadge = '';
   @Input() badgeType = '';
-  attackingType = TYPEATK;
-  defendingType = TYPEDEF;
+  //attackingType = TYPEATK;
+  //defendingType = TYPEDEF;
 
-  getEffectiveness(inputType: string, effectiveness: string): string[] {
+  getAttackEffectiveness(inputType: string, effectiveness: string): string[] {
     let effect: string[];
     switch(effectiveness) {
       case 'superEff': { 
@@ -26,7 +26,7 @@ export class TypeBadgeComponent {
       case 'regEff': {
         if(inputType === "none") {
           effect = [];
-        } else { effect = this.get1xEffectiveness(inputType); }
+        } else { effect = this.get1xAttackEffectiveness(inputType); }
         break;
       }
       case 'notVeryEff': {
@@ -53,7 +53,7 @@ export class TypeBadgeComponent {
     return effect;
   }
 
-  get1xEffectiveness (inputType: string): string[] {
+  get1xAttackEffectiveness(inputType: string): string[] {
     if (inputType === 'none') { return []; }
     let combinedTypes = TYPEATK[inputType].superEff
     .concat(TYPEATK[inputType].notVeryEff)
@@ -62,11 +62,65 @@ export class TypeBadgeComponent {
     return filteredTypes;
   }
 
-  getMultiplier(inputType: string, searchedType: string) {
+  getAttackMultiplier(inputType: string, searchedType: string) {
     if (TYPEATK[inputType].superEff.includes(searchedType)) { return 2; }
     else if (TYPEATK[inputType].notVeryEff.includes(searchedType)) {return 0.5;}
     else if (TYPEATK[inputType].neverEff.includes(searchedType)) { return 0; }
     else { return 1; }
   }
 
+  getDefenseEffectiveness(inputType: string, effectiveness: string): string[] {
+      let effect: string[];
+      switch(effectiveness) {
+        case 'doubleEff': { 
+          if(inputType === "none") {
+            effect = [];
+          } else { effect = TYPEDEF[inputType].superEff; }
+          break;
+        }
+        case 'regEff': {
+          if(inputType === "none") {
+            effect = [];
+          } else { effect = this.get1xDefenseEffectiveness(inputType); }
+          break;
+        }
+        case 'halfEff': {
+          if(inputType === "none") {
+            effect = [];
+          } else { effect = TYPEDEF[inputType].notVeryEff; }
+          break;
+        }
+        case 'neverEff': {
+          if(inputType === "none") {
+            effect = [];
+          } else { effect = TYPEDEF[inputType].neverEff; }
+          break;
+        }
+        case undefined: {
+          effect = [];
+          break;
+        }
+        default: {
+          effect = [];
+          break;
+        }
+      }
+      return effect;
+  }
+
+  get1xDefenseEffectiveness(inputType: string): string[] {
+    if (inputType === 'none') { return []; }
+    let combinedTypes = TYPEDEF[inputType].superEff
+    .concat(TYPEDEF[inputType].notVeryEff)
+    .concat(TYPEDEF[inputType].neverEff);
+    let filteredTypes = TYPES.filter((type) => !combinedTypes.includes(type));
+    return filteredTypes;
+  }
+
+  getDefenseMultiplier(inputType: string, searchedType: string) {
+    if (TYPEDEF[inputType].superEff.includes(searchedType)) { return 2; }
+    else if (TYPEDEF[inputType].notVeryEff.includes(searchedType)) {return 0.5;}
+    else if (TYPEDEF[inputType].neverEff.includes(searchedType)) { return 0; }
+    else { return 1; }
+  }
 }
